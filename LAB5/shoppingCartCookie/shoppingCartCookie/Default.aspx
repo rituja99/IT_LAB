d71415b2c10c9aef94bdc33d499794c5dd0feffc
rituja99 <41865667+rituja99@users.noreply.github.com>
2020-11-01 21:27:21 +0530

    Add files via upload

---
+++ LAB5/shoppingCartCookie/shoppingCartCookie/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" 
+CodeFile="Default.aspx.cs"
+Inherits="shoppingCartCookie.Default" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+	<title>Shopping Cart</title>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div >
+                
+                
+                <asp:Label runat="server" id="userlabel"/> <br/><br/>
+                User Name: <asp:TextBox runat="server" id="username" />
+                <br/><br/>
+                <asp:CheckBoxList id="items" runat="server" />
+                <br/><br/>
+                <asp:Button id="continueShopping" Text="Continue Shopping" OnClick="nextItem" runat="server"/>
+                
+                &nbsp &nbsp &nbsp<asp:Button runat="server" id="ViewList" Text="View List" OnClick="ViewList_Click"/> <br/><br/>
+                <asp:Label runat="server" id="itemsList" />
+                <br/><br/>
+                <asp:Button id="refresh" runat="server" Text="New User" OnClick="NewUser" />
+                
+    </div>
+	</form>
+</body>
+</html>
---
+++ LAB5/shoppingCartCookie/shoppingCartCookie/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace shoppingCartCookie
+{
+    
+    public partial class Default : System.Web.UI.Page
+    {
+        public void Page_Load(object sender, EventArgs args)
+        {
+            if(!IsPostBack)
+            {
+                items.Items.Add(new ListItem("Samsung Galaxy S20", "120000"));
+                items.Items.Add(new ListItem("Samsung Zflip", "180000"));
+                items.Items.Add(new ListItem("Samsung A50s", "21000"));
+                items.Items.Add(new ListItem("iPhone 10", "90000"));
+                items.Items.Add(new ListItem("Samsung M31", "20000"));
+                items.Items.Add(new ListItem("One Plus Nord", "29000"));
+
+            }
+        }
+        public void nextItem(object sender, EventArgs args)
+        {
+            HttpCookie cookie = Request.Cookies[username.Text];
+            if (cookie == null)
+            {
+                cookie = new HttpCookie(username.Text);
+                cookie.Expires = DateTime.Now.AddMinutes(5);
+                cookie["userName"] = username.Text;
+                cookie["cartItem"] = "";
+            }
+            foreach(ListItem item in items.Items)
+            {
+                if(item.Selected)
+                {
+                    cookie["cartItem"] += item.Text + ",";
+                }
+            }
+            Response.Cookies.Add(cookie);
+        }
+        public void ViewList_Click(object sender, EventArgs args)
+        {
+            string[] cartitems;
+            HttpCookie cookie = Request.Cookies[username.Text];
+            if (cookie != null)
+            {
+
+                string itemlist = "Item List: <br/><br/>";
+                userlabel.Text = "Welcome " + cookie["userName"];
+                cartitems = cookie["cartItem"].Split(',');
+                foreach (string ele in cartitems)
+                {
+                    itemlist += ele + "<br/><br/>";
+                }
+                itemsList.Text = itemlist;
+            }
+        }
+        
+        public void NewUser(object sender, EventArgs args)
+        {
+            username.Text = "";
+            itemsList.Text = "";
+            
+        }
+    }
+}
