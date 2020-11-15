395f1ba2317a28ef7fc8efdf30da49257e1430aa
rituja99 <41865667+rituja99@users.noreply.github.com>
2020-11-15 14:56:13 +0530

    Add files via upload

---
+++ LAB9/LAB9Q1/juiceStore.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="juiceStore.aspx.cs" Inherits="LAB9Q1.juiceStore" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            position: absolute;
+            width: 100%;
+            height: 7%;
+            left: 0px;
+            top: 0px;
+        }
+        .auto-style2 {
+            height: 491px;
+            position: absolute;
+            left: 48px;
+            top: 69px;
+            width: 164px;
+            margin-left: 0px;
+        }
+        .auto-style3 {
+            width: 375px;
+            height: 458px;
+            margin-left: 371px;
+            margin-top: 83px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="background-image: url('backpic.jpg'); background-repeat: no-repeat;
+                background-attachment: fixed; background-position: center;background-size:cover; position: absolute;
+                       width: 150%; height: 150%">
+        <div style=" padding: 20px; font-size: xx-large;
+                   font-style: italic; color: #501501; font-family: 'Leelawadee UI'; font-weight: bolder;" class="auto-style1">
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="Label1" runat="server"
+                Font-Underline="True" ForeColor="#FFFFCC" Text="JUICE STORE"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <div style="padding: 30px; text-align: justify; font-size: medium; font-style: italic; color: #FFFFFF;" class="auto-style2">
+
+                Pick your fruits&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:CheckBoxList ID="fruitsList" runat="server" Height="16px" Width="124px">
+                </asp:CheckBoxList>
+                <br />
+                Pick an Ice-Cream<asp:RadioButtonList ID="iceCreamList" runat="server" 
+                    Height="16px" Width="144px">
+                </asp:RadioButtonList>
+                <br />
+                <asp:Button ID="Button1" runat="server" OnClick="display" Width="82px" Text="View Cart" />
+                <br />
+            </div>
+        </div>
+            <div class="auto-style3" style="text-align: center; font-size: large; color: #000000; font-style: italic">
+                <asp:Label ID="Label2" runat="server" Font-Size="X-Large" ForeColor="Black" Font-Bold="True"></asp:Label>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LAB9/LAB9Q1/juiceStore.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LAB9Q1
+{
+    public partial class juiceStore : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                List<ListItem> fruits = new List<ListItem>();
+                fruits.Add(new ListItem("Strawberry","40"));
+                fruits.Add(new ListItem("Blueberry","60"));
+                fruits.Add(new ListItem("Kiwi","70"));
+                fruits.Add(new ListItem("Banana","35"));
+                fruits.Add(new ListItem("Watermelon","45"));
+                fruits.Add(new ListItem("Mango","40"));
+                fruits.Add(new ListItem("Litchi","55"));
+                fruits.Add(new ListItem("Cranberry","60"));
+                fruitsList.DataSource = fruits;
+                fruitsList.DataTextField = "Text";
+                fruitsList.DataValueField = "Value";
+                fruitsList.DataBind();
+
+                List<ListItem> iceCream = new List<ListItem>();
+                iceCream.Add(new ListItem("Vanilla","40"));
+                iceCream.Add(new ListItem("Chocolate","50"));
+                iceCream.Add(new ListItem("Blackcurrent","60"));
+                iceCream.Add(new ListItem("Orea n Cookies","65"));
+                iceCream.Add(new ListItem("Kit-Kat","70"));
+                iceCream.Add(new ListItem("Coffee","65"));
+                iceCream.Add(new ListItem("Butterscotch","50"));
+                iceCreamList.DataTextField = "Text";
+                iceCreamList.DataValueField = "Value";
+                iceCreamList.DataSource = iceCream;
+                iceCreamList.DataBind();
+
+            }
+        }
+
+        protected void display(object sender, EventArgs e)
+        {
+            string str = "Fruits:";
+            int sum = 0;
+            foreach(ListItem ele in fruitsList.Items)
+            {
+                Label2.Text = ele.Value;
+                if(ele.Selected)
+                {
+                    str += "<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + ele.Text ;
+                    sum += Int32.Parse(ele.Value.ToString());
+                }
+            }
+            str += "<br/><br/>Ice-Cream:";
+            foreach(ListItem ele in iceCreamList.Items)
+            {
+                if(ele.Selected)
+                {
+                    str += "<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + ele.Text;
+                    sum += Int32.Parse(ele.Value.ToString());
+                    break;
+                }
+            }
+            str += "<br/><br/>Price:       " + sum.ToString();
+            Label2.Text = str;
+
+        }
+    }
+}
