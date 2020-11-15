cf9327db14f6c1244e891e04705b2c556eb64ad3
rituja99 <41865667+rituja99@users.noreply.github.com>
2020-11-15 14:45:00 +0530

    Add files via upload

---
+++ LAB8/LAB8Q3/items.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="items.aspx.cs" Inherits="LAB8Q3.items" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 395px;
+        }
+    </style>
+</head>
+<body style="background-color: #6AB5FF; background-position:center">
+    <form id="form1" runat="server">
+        <div style="padding: 20px; text-align: center; position: absolute; display: inline-block; width: 100%; height: 100%;">
+            <h1 style="font-style: italic; text-decoration: underline">Product Details</h1>
+            Enter the Price For Vanilla:&nbsp&nbsp<asp:TextBox ID="priceTB" runat="server"></asp:TextBox>
+            <br /><br /><br />
+            <asp:Button ID="update" runat="server" Text="Update" onClick="update_Click"/>
+            <br /><br /><br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+                CellPadding="5" CellSpacing="5" BorderColor="Black" BorderWidth="2"
+                AllowSorting="True" CssClass="auto-style1">
+                <Columns>
+                    <asp:BoundField DataField="ItemId" HeaderText="Item_Id" SortExpression="ItemId" />
+                    <asp:BoundField DataField="flavour" HeaderText="Flavour"  />
+                    <asp:BoundField DataField="price" HeaderText="Price" />
+                    <asp:BoundField DataField="taste" HeaderText="Taste"  />
+                </Columns>
+            </asp:GridView>
+            <br /><br /><br />
+            <asp:Label ID="label1" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ LAB8/LAB8Q3/items.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LAB8Q3
+{
+    public partial class items : System.Web.UI.Page
+    {
+       private string conectionstr
+             = WebConfigurationManager.ConnectionStrings["Products"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           if(!IsPostBack)
+           {
+                display();
+           }
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+           string str = "Update Items Set price=@price where flavour=@flavour";
+           SqlConnection con =new SqlConnection(conectionstr);
+           SqlCommand com=new SqlCommand(str,con);
+           com.Parameters.AddWithValue("@price",priceTB.Text);
+           com.Parameters.AddWithValue("@flavour", "Vanilla");
+            try
+            {
+                con.Open();
+                com.ExecuteNonQuery();
+                display();
+            }         
+            catch(Exception err)
+            {
+                label1.Text = err.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+        protected void display()
+        {
+            string str = "Select * from Items";
+            SqlConnection con = new SqlConnection(conectionstr);
+            SqlCommand com=new SqlCommand(str,con);
+            SqlDataReader reader;
+            try
+            {
+                con.Open();
+                reader = com.ExecuteReader();
+                GridView1.DataSource = reader;
+                GridView1.DataBind();
+
+            }
+            catch(Exception err)
+            {
+                label1.Text = err.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+          
+        }
+    }
+}
