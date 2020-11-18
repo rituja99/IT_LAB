f7412deada206837dc50a6f29c50db356e0a3b03
rituja99 <41865667+rituja99@users.noreply.github.com>
2020-11-18 03:41:41 -0800

    Update Program.cs

--- LAB3/ItemEventHandler/ItemEventHandler/Program.cs
+++ LAB3/ItemEventHandler/ItemEventHandler/Program.cs
@@ -27,35 +27,9 @@ namespace ItemEventHandler
             }
             get { return price; }
         }
-        public Product(string name, decimal price)
-        {
-            Name = name;
-            Price = price;
-        }
-        public Product()
-        {
-
-        }
-    }
-    public class TaxableProduct: Product
-    {
-        private  decimal taxrate = 1.15M;
-
        
-        public TaxableProduct(): base()
-        {
-            
-        }
-        public TaxableProduct(string name, decimal price) : base(name, price)
-        {
-
-        }
-        public decimal totalPrice
-        {
-            get { return taxrate * Price; }
-        }
-
     }
+   
     class Program
     {
         public static void ChangeDetected(decimal price)
@@ -65,20 +39,22 @@ namespace ItemEventHandler
 
         static void Main(string[] args)
         {
-            List<TaxableProduct> items = new List<TaxableProduct>();
+            List<Product> items = new List<Product>();
             string name;
             decimal price;
             for(int i=0;i<3;i++)
             {
                 name = Console.ReadLine();
                 price = Convert.ToDecimal(Console.ReadLine());
-                TaxableProduct item = new TaxableProduct(name, price);
-                item.priceChanged += ChangeDetected;
-                items.Add(item);
+                Product i = new Product();
+                i.Name=name;
+                i.Price = price;
+                i.priceChanged += ChangeDetected;
+                items.Add(i);
             }
-            TaxableProduct itemEvent = items[0];
+            Product itemEvent = items[0];
             itemEvent.Price *= 2;
-            foreach(TaxableProduct item in items)
+            foreach(Product item in items)
             {
                 Console.WriteLine("Name: {0} Total Price: {1}", item.Name, item.totalPrice);
             }
